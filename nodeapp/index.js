697d69cddead010ea9567395beaec27cdd084174
Alberto Salas <alsalasher@gmail.com>
2024-04-07 15:37:27 +0200

    first commit

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -3,7 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1><p>Welcome to my Express.js app.</p>');
+    res.send(`
+    <h1>Hello World!</h1>
+    <p>Bienvenido a mi Express.js app.</p>
+    <a href="/about">About Me</a>
+    `);
+});
+
+app.get('/about', (req, res) => {
+    res.send(`
+    <h1>Página about</h1>
+    <p>Bienvenido a mi página about.</p>
+    `);
 });
 
 app.listen(port, () => {
